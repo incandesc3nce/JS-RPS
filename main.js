02f5a38c741819ebd10d4794e8749d5275d88c9b
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-05-26 12:40:24 +0700

    Handle game completion and add restart feature

--- main.js
+++ main.js
@@ -2,29 +2,43 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
+let roundsPlayed = 0;
+
 rockButton.addEventListener('click', () => {
     const humanChoice = 'rock';
     const computerChoice = getComputerChoice();
+
     const result = playRound(humanChoice, computerChoice);
     updatePicks(humanChoice, computerChoice);
     updateResultText(humanChoice, computerChoice, result);
     updateScore(result);
+
+    roundsPlayed += 1;
+    checkRoundsPlayed(roundsPlayed);
 })
 paperButton.addEventListener('click', () => {
     const humanChoice = 'paper';
     const computerChoice = getComputerChoice();
+
     const result = playRound(humanChoice, computerChoice);
     updatePicks(humanChoice, computerChoice);
     updateResultText(humanChoice, computerChoice, result);
     updateScore(result);
+
+    roundsPlayed += 1;
+    checkRoundsPlayed(roundsPlayed);
 })
 scissorsButton.addEventListener('click', () => {
     const humanChoice = 'scissors';
     const computerChoice = getComputerChoice();
+
     const result = playRound(humanChoice, computerChoice);
     updatePicks(humanChoice, computerChoice);
     updateResultText(humanChoice, computerChoice, result);
     updateScore(result);
+
+    roundsPlayed += 1;
+    checkRoundsPlayed(roundsPlayed);
 })
 
 function getComputerChoice() {
@@ -99,6 +113,39 @@ function updateResultText(humanPick, computerPick, humanWon) {
     resultContainer.textContent = resultText;
 }
 
+function checkRoundsPlayed(rounds) {
+    if (rounds == 5) {
+        endGame();
+        restart()
+        roundsPlayed = 0;
+    }
+}
+
+function endGame() {
+    const humanScore = parseInt(document.querySelector("#human_score").textContent);
+    const computerScore = parseInt(document.querySelector("#comp_score").textContent);
+
+    let message = "";
+
+    if (humanScore > computerScore) {
+        message = "Match ended with your victory!"
+    } else if (humanScore < computerScore) {
+        message = "Match ended with Computer's victory."
+    } else {
+        message = "Match ended with draw."
+    }
+    message += ` Final score is ${humanScore} : ${computerScore}`
+
+    document.querySelector("#result").textContent = message
+}
+
+function restart() {
+    document.querySelector("#human_pick").textContent = '';
+    document.querySelector("#comp_pick").textContent = '';
+
+    document.querySelector("#human_score").textContent = 0;
+    document.querySelector("#comp_score").textContent = 0;
+}
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
